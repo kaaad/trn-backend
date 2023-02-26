a43aaa90c34eac1a7f787bf99665e5b67b78c8de
Kushal Bogati <kushalbogati@Kushals-MacBook-Air.local>
2023-02-26 22:18:25 +0545

    adding comments

--- controllers/merchantCtrl.js
+++ controllers/merchantCtrl.js
@@ -4,6 +4,7 @@ const { generateAccessToken, generateRefreshToken } = require('../utils/generate
 const otpGenerator = require('otp-generator')
 const sendMail = require('../utils/sendEmail')
 const jwt = require('jsonwebtoken')
+const validateMerchant = require('../utils/valid')
 
 
 const merchantCtrl = {
@@ -11,6 +12,7 @@ const merchantCtrl = {
         try {
             const { token, webhook, amount } = req.body
 
+            //decode the jwt token and get the data
             const decoded = jwt.verify(token, `${process.env.ACTIVE_TOKEN}`)
 
             const { merchant } = decoded
@@ -31,6 +33,7 @@ const merchantCtrl = {
         try {
             const { email, password } = req.body
 
+            //get the user data from their email
             const merchant = await Merchants.findOne({ email })
             if(!merchant) return res.status(400).json({msg: "Incorrect email or password"})
 
@@ -47,6 +50,7 @@ const merchantCtrl = {
             const merchant = await Merchants.findOne({ email });
             if(!merchant) return res.status(400).json({msg: "User does not exist"})
 
+            //check if the entered OTP matches with what in the database
             if(merchant && merchant.otp !== otp) {
                 return res.status(400).json({msg: "Invalid OTP"})
             } else {
@@ -62,12 +66,16 @@ const merchantCtrl = {
 module.exports = merchantCtrl;
 
 async function loginUser (merchant, password, res) {
+    //check if the encrypted password matches entered password
     const isMatch = await bcrypt.compare(password, merchant.password)
     if(!isMatch) return res.status(400).json({msg:"Incorrect Email or Password"})
 
+    //generate 6 digit integer code for OTP
     const code = otpGenerator.generate(6, { upperCaseAlphabets: false, lowerCaseAlphabets: false, specialChars: false, digits: true })
 
+    //send OTP via email using nodemailer
     await sendMail(merchant.email, code)
+    //update the otp in database everytime user asks fro OTP
     await Merchants.findOneAndUpdate({_id: merchant._id}, {
         otp: code
     })
--- utils/valid.js
+++ utils/valid.js
@@ -1,4 +1,19 @@
-export function validateEmail(email) {
-    const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-    return re.test(String(email).toLowerCase());
+
+const validateMerchant = (webhook, amount) => {
+    let arr = [];
+
+    if(webhook.length !== 5) {
+        arr.push("Invalid webhook")
+    }
+
+    switch (amount) {
+        case "$500":
+            arr.push("not applicable")
+            break;
+    
+        default:
+            break;
+    }
 }
+
+module.exports = validateMerchant;
